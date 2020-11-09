9485e6d553d8a9d43bcd5420a2426af3f76e0a83
Pedro Vieira <35959018+pmvieira93@users.noreply.github.com>
2020-11-09 22:49:27 +0000

    Initial commit

---
+++ spring-ms-productcatalog/src/main/java/pt/sample/ms/productcatalog/SpringMsProductcatalogApplication.java
@@ -0,0 +1,13 @@
+package pt.sample.ms.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsProductcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMsProductcatalogApplication.class, args);
+	}
+
+}
