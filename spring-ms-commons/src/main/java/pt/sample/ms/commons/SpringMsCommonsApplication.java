0844d77b44c4779bda2aa1eb1163b1ef91efe354
Pedro Vieira <35959018+pmvieira93@users.noreply.github.com>
2020-11-10 22:45:34 +0000

    add commons project e docker compose file

---
+++ spring-ms-commons/src/main/java/pt/sample/ms/commons/SpringMsCommonsApplication.java
@@ -0,0 +1,13 @@
+package pt.sample.ms.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMsCommonsApplication.class, args);
+	}
+
+}
---
+++ spring-ms-commons/src/test/java/pt/sample/ms/commons/SpringMsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.sample.ms.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
