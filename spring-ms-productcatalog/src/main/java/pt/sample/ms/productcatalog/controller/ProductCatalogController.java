78a5dfec3207db6f03346b84356e3876c1523a7a
Pedro Vieira <35959018+pmvieira93@users.noreply.github.com>
2020-11-13 23:28:13 +0000

    Add and Configure ConfigClient Cloud
    Product Catalog
    Added Config Client Cloud Dependency
    Added  Actuator Starter Dependency
    Added Sorted Product List Strategy Dynamically

--- spring-ms-productcatalog/src/main/java/pt/sample/ms/productcatalog/controller/ProductCatalogController.java
+++ spring-ms-productcatalog/src/main/java/pt/sample/ms/productcatalog/controller/ProductCatalogController.java
@@ -3,7 +3,11 @@ package pt.sample.ms.productcatalog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 import pt.sample.ms.commons.entity.Product;
 
 @RestController
+@RefreshScope
 public class ProductCatalogController {
 
 	@Autowired
 	private MongoTemplate mongoTemplate;
+
+	@Value("${product.list.sort.column.default}")
+	private String defaultSortCol;
 
 	@PostMapping("/product")
 	public Product addProduct(@RequestBody Product product) {
@@ -45,7 +53,12 @@ public class ProductCatalogController {
 
 	@GetMapping("/product")
 	public List<Product> getProductList() {
-		return mongoTemplate.findAll(Product.class);
+	    System.out.println("getting product list sorted by " + defaultSortCol);
+	    Query query = new Query();
+	    query.with(Sort.by(Sort.Direction.ASC, defaultSortCol));
+//		return mongoTemplate.findAll(Product.class);
+	    
+	    return mongoTemplate.find(query,Product.class);
 	}
 
 }
